0629ccac95396a3303be25820c616c0b5fb14b5f
Sona Zeynalova <sonie.zeynalova@gmail.com>
2026-03-13 14:26:22 +0400

    Create db.py

---
+++ Employee-Department/db.py
@@ -0,0 +1,21 @@
+import oracledb
+
+def get_connection():
+    return oracledb.connect(
+        user='enda',
+        password='1401',
+        dsn="localhost/orcl21c"
+    )
+
+def create_table():
+    connect = get_connection()
+    cursor = connect.cursor()
+    cursor.execute('''
+        CREATE TABLE employees_department (
+        e_name varchar(20),
+        e_salary int,
+        d_id int )
+    ''')
+    connect.commit()
+    cursor.close()
+    connect.close()
