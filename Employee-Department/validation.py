5db3c5b23919c549b2442701aee0d188ce05b562
Sona Zeynalova <sonie.zeynalova@gmail.com>
2026-03-17 00:52:18 +0400

    Update validation.py

--- Employee-Department/validation.py
+++ Employee-Department/validation.py
@@ -1,12 +1,27 @@
 def is_valid(name, salary, department):
+    alpha = 0
+    for i in name:
+        if i.isalpha():
+            alpha += 1
+    if not alpha:
+        return False
+
+    dep = 0
+    for n in department:
+        if n.isalpha():
+            dep += 1
+    if not dep:
+        return False
+
     if not name:
         print("Name can't be empty")
         return False
-    elif salary < 0:
-        print("Salary can't be negative")
+
+    elif (salary < 0) or (not salary):
+        print("Invalid salary!")
         return False
-    elif department < 0:
-        print("Departament can't be negative")
+    elif not department:
+        print("Department can't be empty")
         return False
     else:
         return True
